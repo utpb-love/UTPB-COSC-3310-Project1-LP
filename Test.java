884a8f3968830e047a5172cb97d12c364334f782
Tim <fielder_t@utpb.edu>
2024-09-30 17:48:55 -0500

    Updated the README file to contain the assignment requirements and grading criteria.

--- Test.java
+++ Test.java
@@ -11,6 +11,9 @@
 public class Test {
     public static void main(String[] args) {
         UInt u1 = new UInt(120);
+        UInt u2 = new UInt(157);
+        UInt u3 = new UInt(212);
+        UInt u4 = new UInt(39);
         System.out.println(u1);
         System.out.println(u1.toInt());
     }
