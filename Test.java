1e50c07e001cd74f1f40b2d658c3d036ecaaf007
Tim <fielder_t@utpb.edu>
2024-09-30 15:13:45 -0500

    Updated UInt to be ready to assign as Project 1.  Added some logic and comments/Javadoc.  Still need to update Test to complete the auto-grader.

--- Test.java
+++ Test.java
@@ -1,3 +1,13 @@
+/**
+ * <h1>Test</h1>
+ * A unit-testing script for UInt.
+ * Ultimately, this is intended to function as an "auto-grader" for the project.
+ * It will check the UInt class to ensure it functions properly and without errors,
+ *   and assign a score to signify whether the class works as intended.
+ *
+ * @author Tim Fielder
+ * @version 0.1 (Sept 30, 2024)
+ */
 public class Test {
     public static void main(String[] args) {
         UInt u1 = new UInt(120);
--- UInt.java
+++ UInt.java
@@ -1,26 +1,54 @@
+/**
+ * @auth
+ */
+
 import java.util.Arrays;
 
+/**
+ * <h1>UInt</h1>
+ * Represents an unsigned integer using a boolean array to store the binary representation.
+ * Each bit is stored as a boolean value, where true represents 1 and false represents 0.
+ *
+ * @author Tim Fielder
+ * @version 1.0 (Sept 30, 2024)
+ */
 public class UInt {
 
+    // The array representing the bits of the unsigned integer.
     protected boolean[] bits;
+
+    // The number of bits used to represent the unsigned integer.
     protected int length;
 
-    public UInt() {
-
-    }
-
+    /**
+     * Constructs a new UInt by cloning an existing UInt object.
+     *
+     * @param toClone The UInt object to clone.
+     */
     public UInt(UInt toClone) {
         this.length = toClone.length;
         this.bits = Arrays.copyOf(toClone.bits, this.length);
     }
 
+    /**
+     * Constructs a new UInt from an integer value.
+     * The integer is converted to its binary representation and stored in the bits array.
+     *
+     * @param i The integer value to convert to a UInt.
+     */
     public UInt(int i) {
+        // Determine the number of bits needed to store i in binary format.
         length = (int)(Math.ceil(Math.log(i)/Math.log(2.0)) + 1);
         bits = new boolean[length];
 
+        // Convert the integer to binary and store each bit in the array.
         for (int b = length-1; b >= 0; b--) {
+            // We use a ternary to decompose the integer into binary digits, starting with the 1s place.
             bits[b] = i % 2 == 1;
+            // Right shift the integer to process the next bit.
             i = i >> 1;
+
+            // Deprecated analog method
             /*int p = 0;
             while (Math.pow(2, p) < i) {
                 p++;
@@ -31,82 +59,162 @@ public class UInt {
         }
     }
 
+    /**
+     * Creates and returns a copy of this UInt object.
+     *
+     * @return A new UInt object that is a clone of this instance.
+     */
     @Override
     public UInt clone() {
         return new UInt(this);
     }
 
+    /**
+     * Creates and returns a copy of the given UInt object.
+     *
+     * @param u The UInt object to clone.
+     * @return A new UInt object that is a copy of the given object.
+     */
     public static UInt clone(UInt u) {
         return new UInt(u);
     }
 
+    /**
+     * Converts this UInt to its integer representation.
+     *
+     * @return The integer value corresponding to this UInt.
+     */
     public int toInt() {
         int t = 0;
+        // Traverse the bits array to reconstruct the integer value.
         for (int i = 0; i < length; i++) {
+            // Again, using a ternary to now re-construct the int value, starting with the most-significant bit.
             t = t + (bits[i] ? 1 : 0);
+            // Shift the value left for the next bit.
             t = t << 1;
         }
-        return t >> 1;
+        return t >> 1; // Adjust for the last shift.
     }
 
+    /**
+     * Static method to retrieve the int value from a generic UInt object.
+     *
+     * @param u The UInt to convert.
+     * @return The int value represented by u.
+     */
+    public static int toInt(UInt u) {
+        return u.toInt();
+    }
+
+    /**
+     * Returns a String representation of this binary object with a leading 0b.
+     *
+     * @return The constructed String.
+     */
     @Override
     public String toString() {
-        String s = "0b";
+        StringBuilder s = new StringBuilder("0b");
+        // Construct the String starting with the most-significant bit.
         for (int i = 0; i < length; i++) {
-            s += bits[i] ? "1" : "0";
+            // Again, we use a ternary here to convert from true/false to 1/0
+            s.append(bits[i] ? "1" : "0");
         }
-        return s;
+        return s.toString();
     }
 
-    public static int toInt(UInt u) {
-        return 0;
-    }
-
+    /**
+     * Performs a logical AND operation using this.bits and u.bits, with the result stored in this.bits.
+     *
+     * @param u The UInt to AND this against.
+     */
     public void and(UInt u) {
-        return;
+        // We want to traverse the bits arrays to perform our AND operation.
+        // But keep in mind that the arrays may not be the same length.
+        // So first we use Math.min to determine which is shorter.
+        // Then we need to align the two arrays at the 1s place, which we accomplish by indexing them at length-i-1.
+        for (int i = 0; i < Math.min(this.length, u.length); i++) {
+            this.bits[this.length - i - 1] =
+                    this.bits[this.length - i - 1] &
+                            u.bits[u.length - i - 1];
+        }
     }
 
+    /**
+     * Accepts a pair of UInt objects and uses a temporary clone to safely AND them together (without changing either).
+     *
+     * @param a The first UInt
+     * @param b The second UInt
+     * @return The temp object containing the result of the AND op.
+     */
     public static UInt and(UInt a, UInt b) {
-        return null;
+        UInt temp = a.clone();
+        temp.add(b);
+        return temp;
     }
 
     public void or(UInt u) {
+        // TODO Complete the bitwise logical OR method
         return;
     }
 
     public static UInt or(UInt a, UInt b) {
+        // TODO Complete the static OR method
         return null;
     }
 
     public void xor(UInt u) {
+        // TODO Complete the bitwise logical XOR method
         return;
     }
 
     public static UInt xor(UInt a, UInt b) {
+        // TODO Complete the static XOR method
         return null;
     }
 
     public void add(UInt u) {
+        // TODO Using a ripple-carry adder, perform addition using a passed UINT object
+        // The result will be stored in this.bits
+        // You will likely need to create a couple of helper methods for this.
+        // Note this one, like the bitwise ops, also needs to be aligned on the 1s place.
+        // Also note this may require increasing the length of this.bits to contain the result.
         return;
     }
 
     public static UInt add(UInt a, UInt b) {
+        // TODO A static change-safe version of add, should return a temp UInt object like the bitwise ops.
         return null;
     }
 
+    public void negate() {
+        // TODO You'll need a way to perform 2's complement negation
+        // The add() method will be helpful with this.
+    }
+
     public void sub(UInt u) {
+        // TODO Using negate() and add(), perform in-place subtraction
+        // As this class is supposed to handle only unsigned values,
+        //   if the result of the subtraction operation would be a negative number then it should be coerced to 0.
         return;
     }
 
     public static UInt sub(UInt a, UInt b) {
+        // TODO And a static change-safe version of sub
         return null;
     }
 
     public void mul(UInt u) {
+        // TODO Using Booth's algorithm, perform multiplication
+        // This one will require that you increase the length of bits, up to a maximum of X+Y.
+        // Having negate() and add() will obviously be useful here.
+        // Also note the Booth's always treats binary values as if they are signed,
+        //   while this class is only intended to use unsigned values.
+        // This means that you may need to pad your bits array with a leading 0 if it's not already long enough.
         return;
     }
 
     public static UInt mul(UInt a, UInt b) {
+        // TODO A static, change-safe version of mul
         return null;
     }
 }
